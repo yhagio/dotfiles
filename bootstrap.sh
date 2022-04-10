d8eb0149eed00b288833c5cf663d7d2486b69bb7
Yuichi Hagio <yhagio87@gmail.com>
2022-04-10 17:15:11 -0400

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,8 +8,8 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 DOTFILE_DIR = $(pwd)
 
 # echo "Setting up symlinks..."
-# ln -sfn $DOTFILE_DIR/local_infra ~/.local_infra
-# ln -sfn $DOTFILE_DIR/shopify_stuff ~/.shopify_stuff
+ln -sfn $DOTFILE_DIR/local_infra ~/.local_infra
+ln -sfn $DOTFILE_DIR/shopify_stuff ~/.shopify_stuff
 ln -sfn $DOTFILE_DIR/zshrc ~/.zshrc 
 # ln -sfn $DOTFILE_DIR/gitconfig ~/.gitconfig
 # Reference ln command: https://man7.org/linux/man-pages/man1/ln.1.html
