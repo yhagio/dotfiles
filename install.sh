906bc068a9fd9997b58cafd552f9ad6fdb8d9784
Yuichi Hagio <yhagio87@gmail.com>
2022-04-10 16:28:15 -0400

    update

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ echo "Installing oh-my-zsh ..."
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
 
 echo "Setting up symlinks..."
-ln -sfn $THIS_DIR/local_infra ~/.local_infra
-ln -sfn $THIS_DIR/shopify_stuff ~/.shopify_stuff
-ln -sfn $THIS_DIR/zshrc ~/.zshrc 
-ln -sfn $THIS_DIR/gitconfig ~/.gitconfig
+ln -sf $THIS_DIR/local_infra ~/.local_infra
+ln -sf $THIS_DIR/shopify_stuff ~/.shopify_stuff
+ln -sf $THIS_DIR/zshrc ~/.zshrc 
+ln -sf $THIS_DIR/gitconfig ~/.gitconfig
 # Reference ln command: https://man7.org/linux/man-pages/man1/ln.1.html
 
 source ~/.zshrc
